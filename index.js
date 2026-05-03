ddfa459a8eddbcab77f565126ce26891a114bdc2
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:42:41 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// markenportal - Markenportal - Zentrales Portal fuer Markenverwaltung und IP-Management. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
